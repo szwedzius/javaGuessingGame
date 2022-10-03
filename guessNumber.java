0e8a57988abcda8ba55224a0bfb9095d838042ac
szwedzius <jakubszweda1@gmail.com>
2022-10-03 12:34:17 -0400

    Guess phase works

--- guessNumber.java
+++ guessNumber.java
@@ -4,19 +4,33 @@ import java.util.Random;
 public class guessNumber{
 	public static void main(String[] args){
 		System.out.println("I will pick a random number between 1 and your number");
+		System.out.println("If u pick 0 then I end the game");
 		System.out.println("What is your number?");
 		Scanner sc = new Scanner(System.in);
 		int upper = sc.nextInt();
-		System.out.format("Your number is %d",upper);
+		if(upper == 0) return;
+		System.out.format("Your number is %d \n",upper);
 
 		int randomNumber = random(upper);
-		System.out.format("The randomly selected number is %d", randomNumber);
+		guess(randomNumber, sc);
 	}
 	//Random number generator
 	//in the range of <1, upper>
 	public static int random(int upper){
 		Random rand = new Random();
 		return rand.nextInt(upper) + 1;
+	}
+
+	public static void guess(int value, Scanner sc){
+		int attempt = 0;
+		int attemptCount = 0;
+		do{
+			System.out.println("Try to guess my number");
+			attempt = sc.nextInt();
+			attemptCount++;
+		} while(attempt != value);
+		System.out.println("Congratulations!!!");
+		System.out.format("It only took you %d tries", attemptCount);
 	}
 
 }
