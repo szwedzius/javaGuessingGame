cde87ae6d4964e6ce23335ae2ba95ab495d650ec
szwedzius <jakubszweda1@gmail.com>
2022-10-03 12:38:17 -0400

    Final changes

--- guessNumber.java
+++ guessNumber.java
@@ -4,12 +4,11 @@ import java.util.Random;
 public class guessNumber{
 	public static void main(String[] args){
 		System.out.println("I will pick a random number between 1 and your number");
-		System.out.println("If u pick 0 then I end the game");
+		System.out.println("If u pick 0 then I end the game \n");
 		System.out.println("What is your number?");
 		Scanner sc = new Scanner(System.in);
 		int upper = sc.nextInt();
 		if(upper == 0) return;
-		System.out.format("Your number is %d \n",upper);
 
 		int randomNumber = random(upper);
 		guess(randomNumber, sc);
@@ -24,11 +23,12 @@ public class guessNumber{
 	public static void guess(int value, Scanner sc){
 		int attempt = 0;
 		int attemptCount = 0;
-		do{
+		do {
 			System.out.println("Try to guess my number");
 			attempt = sc.nextInt();
 			attemptCount++;
 		} while(attempt != value);
+
 		System.out.println("Congratulations!!!");
 		System.out.format("It only took you %d tries", attemptCount);
 	}
